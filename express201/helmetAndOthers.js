f6da71dd6a9a08e2936af7110d85030c11454ac1
Yihao <yesiamyihao@gmail.com>
2021-05-02 15:52:47 +1200

    Add helmetAndOthers.js

---
+++ express201/helmetAndOthers.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const helmet = require('helmet');
+const app = express();
+
+app.use(helmet());
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/ajax', (req, res, next) => {
+    console.log(req);
+    res.send('Test');
+});
+
+app.listen(3000, () => console.log('Listening on port 3000...'));
