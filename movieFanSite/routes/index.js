0038efc857634ba9bad12fd3eecaa1f1a08b79c9
Yihao <yesiamyihao@gmail.com>
2021-07-17 01:36:07 +1200

    Fetch data from tmdb api

--- movieFanSite/routes/index.js
+++ movieFanSite/routes/index.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
+
+const apiKey = '4cdfed3bf08c9bd60161d850d68007c4';
+const apiBaseUrl = 'http://api.themoviedb.org/3';
+const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
+const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  request.get(nowPlayingUrl, (err, res, data) => {
+    const movieData = JSON.parse(data);
+    console.log(movieData);
+  });
   res.render('index', { title: 'Express' });
 });
 
